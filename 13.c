f5171273758c51f729711e826162f650f4a2914c
Mahesh Rathod <118987463+mahesh95-git@users.noreply.github.com>
2023-03-18 13:56:59 +0530

    Update 13.c

--- 13.c
+++ 13.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
 #include<conio.h>
+void main(){
+FILE*fp=NULL;
+char  cout;
+int i=0;
+clrscr();
+fp=fopen("m.text","r");
+if(fp==NULL){
 
-int main(){
-return 0;}
+printf("file not exits");
+}
+while((cout=fgetc(fp))!=EOF){
+i++;
+
+}
+printf("%d",i);
+getch();}
