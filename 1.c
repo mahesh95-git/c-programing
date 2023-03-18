4e4cc5838859abac9873e1cfa388f73bf7925c55
Mahesh Rathod <118987463+mahesh95-git@users.noreply.github.com>
2023-03-18 13:41:41 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
 void sum(int a,int b){
-int sum=0;
-    printf("addition of tow number is:%d",sum=a+b);
+int multi=0;
+    printf("multiplication of tow number is:%d",multi=a+b);
 }
 void main(){
     int a,b;
 printf("function with arguments and without return values");
-printf("\nprogram to addition two number");
+printf("\nprogram to multiplication two number");
 printf("\nenter tow number:");
 scanf("%d%d",&a,&b);
-sum(a,b);
+multi(a,b);
     
 }
