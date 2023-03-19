607a6cf1119de1e4606520008cc2445c696d6d1e
Mahesh Rathod <118987463+mahesh95-git@users.noreply.github.com>
2023-03-19 20:17:13 +0530

    Update 10.c

--- 10.c
+++ 10.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<conio.h>
 void factorial(int a){
     int fact=1;
   for(int i=1;i<=a;i++){
@@ -7,10 +7,10 @@ void factorial(int a){
   }
 printf("factoril of given number is:%d",fact);
 }
-int main(){
+void main(){
     int a;
     printf("Write a program to calculate factorial of given number by using user defined function");
 printf("\nenter a number:");
 scanf("%d",&a);
 factorial(a);
-return 0;}
+getch();}
