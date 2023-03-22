c93b0df5516697cdd2da5a523decc9e9bb8aad43
Mahesh Rathod <118987463+mahesh95-git@users.noreply.github.com>
2023-03-22 16:34:34 +0530

    Update 8.2.c

--- 8.2.c
+++ 8.2.c
@@ -4,6 +4,7 @@ void swap(int *, int *);
 void main()
 {
     int a, b;
+    clrscr();
  printf("Write a program which swap two number using a call by reference\n");
     printf("program for call by reference");
     printf("\n enter any two numbers");
@@ -12,7 +13,7 @@ void main()
     swap(&a, &b);
     printf("\n values after swapping\n No1=%d \t No2=%d", a, b);
    
-}
+getch();}
 void swap(int *a, int *b)
 {
     int temp;
